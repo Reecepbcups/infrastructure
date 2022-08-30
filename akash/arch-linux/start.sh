046d445b6c1881552341c29747152b937b6bce66
Reece Williams <reecepbcups@gmail.com>
2022-08-30 12:51:05 -0500

    arch update

--- akash/arch-linux/start.sh
+++ akash/arch-linux/start.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 source $HOME/.bashrc
-pacman --noconfirm -Syyu go git wget curl nginx
+source $HOME/.bash_profile
+
+# Timezone
+timedatectl set-timezone America/Chicago
+
+# Install
+pacman --noconfirm -Syyu openssh sudo go git wget curl nginx base-devel
+
+# non core installs
+pacman --noconfirm -Syyu python-pip zip unzip lsof jq dos2unix btop nano
+
+# Crontab
+# pacman --noconfirm -Syyu cron
+# systemctl enable cronie && systemctl start cronie
+
+# Performance Increase
+echo "65535" > /proc/sys/fs/file-max
+echo "fs.file-max = 65535" >> /etc/sysctl.conf
+echo "root hard nofile 150000" >> /etc/security/limits.conf
+echo "root soft nofile 150000" >> /etc/security/limits.conf
+echo "* hard nofile  150000" >> /etc/security/limits.conf
+echo "* soft nofile 150000" >> /etc/security/limits.conf
+
+
 sudo touch /etc/ssh/sshd_config
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 (echo ${my_root_password}; echo ${my_root_password}) | passwd root
-systemctl restart sshd
-systemctl start nginx
-sleep 5
-pacman -Su nano --noconfirm
-runsvdir -P /etc/service &
-source $HOME/.bashrc
+# systemctl restart sshd
+# systemctl start nginx
+service sshd restart
+service nginx start
+# runsvdir -P /etc/service &
 
