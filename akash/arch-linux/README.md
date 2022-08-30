c53be8f7be6fb4a18666a55d14ccd03cb695332c
Reece Williams <reecepbcups@gmail.com>
2022-08-30 12:34:02 -0500

    new script

--- akash/arch-linux/README.md
+++ akash/arch-linux/README.md
@@ -1,6 +1,7 @@
 # Arch on Akash
+Fork from: https://github.com/nfl0/zcashd-akash
 
-https://github.com/chalabi2/ubuntu-akash
+Create lease via CLoudmos tool.
 
-
-main: https://github.com/nfl0/zcashd-akash
+ssh root@5uo6vs083l803f6bndtdd45bg0.ingress.example.com -p 31573
+where P = your port forwarded from 22
--- akash/arch-linux/start.sh
+++ akash/arch-linux/start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 source $HOME/.bashrc
 pacman --noconfirm -Syyu go git wget curl nginx
+sudo touch /etc/ssh/sshd_config
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 (echo ${my_root_password}; echo ${my_root_password}) | passwd root
 systemctl restart sshd
 systemctl start nginx
 sleep 5
-pacman -Su nano
+pacman -Su nano --noconfirm
 runsvdir -P /etc/service &
 source $HOME/.bashrc
 
