166d82db532ae61bbe8cbb7519b48205b017bc2a
Reece Williams <reecepbcups@gmail.com>
2022-08-30 13:45:45 -0500

    readme,s tart.sh

--- akash/arch-linux/README.md
+++ akash/arch-linux/README.md
@@ -1,7 +1,15 @@
 # Arch on Akash
-Fork from: https://github.com/nfl0/zcashd-akash
 
-Create lease via CLoudmos tool.
+
+> Create a new Deployment with Cloudmos on Akash
+> Modify the deploy.yaml to your needs
+> Get the URL & PORT which was forwarded, use those to SSH in.
 
 ssh root@5uo6vs083l803f6bndtdd45bg0.ingress.example.com -p 31573
-where P = your port forwarded from 22
+
+For best security, you should:
+```
+- ssh-copy-id root@akash_host -p PORT
+- disable PermitRootLogin in /etc/ssh/sshd_config
+- create a user with their own home directory.
+```
--- akash/arch-linux/start.sh
+++ akash/arch-linux/start.sh
@@ -9,9 +9,9 @@ timedatectl set-timezone America/Chicago
 pacman --noconfirm -Syyu openssh sudo go git wget curl nginx base-devel
 
 # non core installs
-pacman --noconfirm -Syyu python-pip zip unzip lsof jq dos2unix btop nano cron cronie
+pacman --noconfirm -Syyu python-pip zip unzip lsof jq dos2unix btop nano cron cronie neofetch
 
-# Performance Increase
+# File increase limits
 echo "65535" > /proc/sys/fs/file-max
 echo "fs.file-max = 65535" >> /etc/sysctl.conf
 echo "root hard nofile 150000" >> /etc/security/limits.conf
@@ -19,17 +19,19 @@ echo "root soft nofile 150000" >> /etc/security/limits.conf
 echo "* hard nofile  150000" >> /etc/security/limits.conf
 echo "* soft nofile 150000" >> /etc/security/limits.conf
 
+# Change password
 (echo ${my_root_password}; echo ${my_root_password}) | passwd root
 
+# Gernerate SSH keys & setup sshd
 ssh-keygen -A
 sudo touch /etc/ssh/sshd_config
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-sed -i -e 's/^UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config # not tested yet
+sed -i -e 's/^UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
 
-# you have to start directly from the files, you cant systemctl / service
-# start ssh in the background on port 22
+# You have to start directly from the files, you cant systemctl / service bc systemd things
 /usr/sbin/sshd -p 22 -f /etc/ssh/sshd_config
 /usr/bin/nginx -g 'pid /run/nginx.pid; error_log stderr;'
 /usr/sbin/crond -i
 
+# makes the directory we put persistant storage on, /root/storage
 mkdir -p /root/storage/
